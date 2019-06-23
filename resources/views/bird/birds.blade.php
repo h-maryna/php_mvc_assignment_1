1a0c29f4b8e1675cd405e2002490e31046bb7f93
Maryna <marynahaidashevska@gmail.com>
2019-06-22 23:59:06 -0500

    PHP MVC ASSIGNMENT 1 COMPLETED

--- resources/views/bird/birds.blade.php
+++ resources/views/bird/birds.blade.php
@@ -12,13 +12,16 @@ consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
+<h2>Birds</h2>
+  
+  <ol>
   @foreach($birds as $bird)
   <div class="list-group">
-  	<a href="/bird/birds/{{$bird->id}}" class="list-group-item list-group-item-action">{{ $bird->name }}</a>
-  
+  	<li><a href="/bird/birds/{{$bird->id}}" class="list-group-item list-group-item-action">{{ $bird->name }}</a>
+  </li>
 </div>
 @endforeach
-
+</ol>
 
 @endsection
 
